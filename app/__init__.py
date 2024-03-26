b47118c3832f42a5372321b32a752a391b7645dd
Brian <bstanton773@gmail.com>
2024-03-26 14:34:37 -0500

    Setup Flask-SQLAlchemy and configure

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask # Import the Flask class from the flask module
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 # Create an instance of Flask called app which will be the central object
 app = Flask(__name__)
+# Set the configuration for the app
+app.config.from_object(Config)
+
+# Create an instance of SQLAlchemy called db which be the cental object for our database
+db = SQLAlchemy(app)
 
 # import the routes to the app
 from . import routes
---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+# Get the base directory of this folder
+basedir = os.path.abspath(os.path.dirname(__file__))
+# "C:\Users\bstan\Documents\codingtemple-kekambas-142\week6\flask-blog-api"
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
